781d4a8542e7513d563c73ad2de1ea151b5c430e
liqingshan <1182102704@qq.com>
2016-11-04 17:59:48 +0800

    fxi

--- app/src/main/java/com/androidlee/api/TestService.java
+++ app/src/main/java/com/androidlee/api/TestService.java
@@ -1,9 +1,20 @@
 package com.androidlee.api;
 
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
 /**
  * 作者： 李青山
  * 创建时间： 2016/11/4.
  */
 
 public interface TestService {
+    //get请求用（Query）拼接参数
+    //post用（Field）拼接参数id=1463129652953459
+    @FormUrlEncoded
+    @POST("imageManage/findPicInfoByIdForAPP.action")
+    Call<String> addReviews(@Field("ph_id") String bookId
+    );
 }
