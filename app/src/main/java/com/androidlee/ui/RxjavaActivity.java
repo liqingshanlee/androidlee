18c756b135105e725496000b6930ffe0c89701c7
liqingshan <1182102704@qq.com>
2016-11-15 15:42:43 +0800

    fix

--- app/src/main/java/com/androidlee/ui/RxjavaActivity.java
+++ app/src/main/java/com/androidlee/ui/RxjavaActivity.java
@@ -5,6 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 
 import com.androidlee.R;
 
+import rx.Observable;
+import rx.Subscriber;
+
 /**
  * 作者： 李青山
  * 创建时间： 2016/11/15.
@@ -17,5 +20,13 @@ public class RxjavaActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.rxjava);
     }
-
+    Observable observable = Observable.create(new Observable.OnSubscribe<String>() {
+        @Override
+        public void call(Subscriber<? super String> subscriber) {
+            subscriber.onNext("Hello");
+            subscriber.onNext("Hi");
+            subscriber.onNext("Aloha");
+            subscriber.onCompleted();
+        }
+    });
 }
