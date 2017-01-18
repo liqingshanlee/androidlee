35f5d8d0dfdd0a09e068ba2149bafd6473374346
liqingshan <1182102704@qq.com>
2017-01-18 16:06:03 +0800

    fix

--- app/src/main/java/com/androidlee/ui/RxjavaActivity1.java
+++ app/src/main/java/com/androidlee/ui/RxjavaActivity1.java
@@ -5,7 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import com.androidlee.R;
+import com.androidlee.rxjava2.RxUtils;
 
 
 /**
@@ -13,23 +15,38 @@ import com.androidlee.R;
  * 创建时间： 2016/12/14.
  */
 
-public class RxjavaActivity1 extends AppCompatActivity implements View.OnClickListener {
+public class RxjavaActivity1 extends AppCompatActivity {
     TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.rxjava1);
-        mTextView = (TextView) findViewById(R.id.test_rxjava2);
-        mTextView.setOnClickListener(this);
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.test_rxjava2:
-                Toast.makeText(RxjavaActivity1.this, "测试", Toast.LENGTH_SHORT).show();
-                break;
-        }
+
+    /**
+     * 调用create 方法
+     *
+     * @param view
+     */
+    public void createMethod(View view) {
+        RxUtils.createObserable();
+    }
+
+    public void createMethod2(View view) {
+        RxUtils.createPrint();
+    }
+
+    public void from(View view) {
+        RxUtils.from();
+    }
+
+    public void just(View view) {
+        RxUtils.just();
+    }
+
+    public void filter(View view) {
+        RxUtils.filter();
     }
 }
